78b0cbc430b22efe15f3a40c5a53d074bddfed54
IosdanFerreira <103718877+IosdanFerreira@users.noreply.github.com>
2023-08-03 00:41:11 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export'
+  experimental: {
+    images: true,
+  },
 }
 
 module.exports = nextConfig
